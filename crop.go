05c862b1d465c56d7eaf76c4977a15975e6bb8a8
John Wang <johncwang@gmail.com>
2022-01-05 05:47:14 -0800

    style: crop: rename param name

--- crop.go
+++ crop.go
@@ -12,7 +12,7 @@ import (
 
 // CropImage takes an image and crops it to the specified rectangle. `CropImage`
 // is from: https://stackoverflow.com/a/63256403.
-func CropImage(img image.Image, crop image.Rectangle) (image.Image, error) {
+func CropImage(img image.Image, retain image.Rectangle) (image.Image, error) {
 	type subImager interface {
 		SubImage(r image.Rectangle) image.Image
 	}
@@ -25,7 +25,7 @@ func CropImage(img image.Image, crop image.Rectangle) (image.Image, error) {
 		return nil, fmt.Errorf("image does not support cropping")
 	}
 
-	return simg.SubImage(crop), nil
+	return simg.SubImage(retain), nil
 }
 
 func CropImageColor(img image.Image, tolerance float64, remove ...color.Color) (image.Image, error) {
