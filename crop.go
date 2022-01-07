fcad2d6ced5db4d7bbfb561a226bd4b814bf9720
John Wang <johncwang@gmail.com>
2022-01-07 04:54:34 -0800

    feat: crop: add `CropImageColorCaption()`, `RowsFilter()`, `RowMatch()`, `RowMatchCaption()`

--- crop.go
+++ crop.go
@@ -67,6 +67,44 @@ func CropImageColor(img image.Image, tolerance float64, remove ...color.Color) (
 	return CropImage(img, newRect)
 }
 
+func CropImageColorCaption(img image.Image, paddingPct, tolerance float64, remove ...color.Color) (image.Image, error) {
+	rows := RowsFilterCaption(img, paddingPct, tolerance, remove...)
+	if len(rows) == 0 {
+		return img, nil
+	}
+
+	newRect := img.Bounds().Bounds()
+
+	if int(rows[len(rows)-1]) == img.Bounds().Max.Y-1 {
+		trimBottom := 0
+		for y := len(rows) - 1; y >= 0; y-- {
+			if y == 0 {
+				trimBottom = int(rows[0])
+			} else if rows[y] == rows[y-1]+1 {
+				trimBottom = int(rows[y-1])
+			} else {
+				break
+			}
+		}
+		validatedTrimBottom := 0
+		for y := trimBottom; y < img.Bounds().Max.Y; y++ {
+			if RowMatch(img, y, tolerance, remove...) {
+				validatedTrimBottom = y
+				break
+			}
+		}
+		newRect.Max.Y = validatedTrimBottom
+	}
+	if newRect.Eq(img.Bounds()) {
+		return img, nil
+	}
+
+	if newRect.Eq(img.Bounds()) {
+		return img, nil
+	}
+	return CropImage(img, newRect)
+}
+
 // ColumnsFilter returns a list of column indexes that matches the wanted colors
 // within the provided tolerane.
 func ColumnsFilter(img image.Image, tolerance float64, want ...color.Color) []uint {
@@ -88,4 +126,112 @@ func ColumnsFilter(img image.Image, tolerance float64, want ...color.Color) []ui
 		}
 	}
 	return cols
+}
+
+// RowsFilter returns a list of row indexes that matches the wanted colors
+// within the provided tolerane.
+func RowsFilter(img image.Image, tolerance float64, want ...color.Color) []uint {
+	rows := []uint{}
+	if len(want) == 0 {
+		return rows
+	}
+	wantColorsUnique := micolors.ColorsDistance(colors.SliceUnique(want))
+	minPt := img.Bounds().Min
+	maxPt := img.Bounds().Max
+	for y := minPt.Y; y < maxPt.Y; y++ {
+		rowColors := []color.Color{}
+		for x := minPt.X; x < maxPt.X; x++ {
+			rowColors = append(rowColors, img.At(x, y))
+		}
+		colColorsUniqueDist := micolors.ColorsDistance(colors.SliceUnique(rowColors))
+		if wantColorsUnique.MatchBest(tolerance, colColorsUniqueDist...) {
+			rows = append(rows, uint(y))
+		}
+	}
+	return rows
+}
+
+// RowMatch checks to see if an image row matches the want colors.
+func RowMatch(img image.Image, rowIdx int, tolerance float64, want ...color.Color) bool {
+	if rowIdx <= 0 {
+		return false
+	}
+	wantColorsUnique := micolors.ColorsDistance(colors.SliceUnique(want))
+	minPt := img.Bounds().Min
+	maxPt := img.Bounds().Max
+	if rowIdx >= maxPt.Y {
+		return false
+	}
+	y := rowIdx
+	rowColors := []color.Color{}
+	for x := minPt.X; x < maxPt.X; x++ {
+		rowColors = append(rowColors, img.At(x, y))
+	}
+	colColorsUniqueDist := micolors.ColorsDistance(colors.SliceUnique(rowColors))
+	return wantColorsUnique.MatchBest(tolerance, colColorsUniqueDist...)
+}
+
+// RowsFilterCaption returns a list of column indexes that matches the wanted colors
+// within the provided tolerane.
+func RowsFilterCaption(img image.Image, paddingPct, tolerance float64, want ...color.Color) []uint {
+	rows := []uint{}
+	if len(want) == 0 {
+		return rows
+	}
+	wantColorsUnique := micolors.ColorsDistance(colors.SliceUnique(want))
+	minPt := img.Bounds().Min
+	maxPt := img.Bounds().Max
+	for y := minPt.Y; y < maxPt.Y; y++ {
+		rowColors := []color.Color{}
+		for x := minPt.X; x < maxPt.X; x++ {
+			rowColors = append(rowColors, img.At(x, y))
+		}
+		if RowMatchCaption(paddingPct, tolerance, wantColorsUnique, rowColors) {
+			rows = append(rows, uint(y))
+		}
+	}
+	return rows
+}
+
+// RowCaptionMatch matches a row with a caption. paddingPct is what is considered for matching.
+// padding is left or right. Padding 0.5 or greater means matching the entire row. Padding
+// <= 0 means no matching is necessary to succeed. Negative padding is not supported and converted
+// to 0.
+func RowMatchCaption(paddingPct, tolerance float64, wantColorsUnique micolors.ColorsDistance, candidate []color.Color) bool {
+	if len(wantColorsUnique) == 0 || len(candidate) == 0 {
+		return false
+	} else if paddingPct <= 0 {
+		return true
+	}
+	if paddingPct >= 0.5 {
+		for _, cx := range candidate {
+			if !wantColorsUnique.MatchBest(tolerance, cx) {
+				return false
+			}
+		}
+		return true
+	}
+	candidateCount := len(candidate)
+	paddingCount := int(float64(candidateCount) * paddingPct)
+	// no padding to match
+	if paddingCount == 0 {
+		return true
+	}
+	// try left
+	for x := 0; x <= paddingCount; x++ {
+		cx := candidate[x]
+		if !wantColorsUnique.MatchBest(tolerance, cx) {
+			return false
+		}
+	}
+	// try right
+	// len = 5, p = 1  x = 4, succ, test >= 5-1 = 4; succ
+	// len = 5, p = 1, x = 3, fail, test >= 5-1 = 4; fail
+	for x := len(candidate) - 1; x >= len(candidate)-paddingCount; x-- {
+		cx := candidate[x]
+		if !wantColorsUnique.MatchBest(tolerance, cx) {
+			return false
+		}
+	}
+	return true
 }
