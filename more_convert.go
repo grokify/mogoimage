54538fcf7c9a564e70cd2da38664aa27cf036ae8
John Wang <johncwang@gmail.com>
2022-02-19 23:21:33 -0800

    feat: add `ToPalettedMedianCut()`

---
+++ more_convert.go
@@ -0,0 +1,17 @@
+package mogoimage
+
+import (
+	"image"
+
+	"github.com/andybons/gogif"
+)
+
+func ToPalettedMedianCut(src image.Image) *image.Paletted {
+	if v, ok := src.(*image.Paletted); ok {
+		return v
+	}
+	pimg := image.NewPaletted(src.Bounds(), nil)
+	quantizer := gogif.MedianCutQuantizer{NumColor: 256}
+	quantizer.Quantize(pimg, src.Bounds(), src, image.ZP)
+	return pimg
+}
