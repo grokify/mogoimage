a189ba34dd2773b9a23dda6b315867cc094d26d5
John Wang <johncwang@gmail.com>
2023-06-24 02:23:02 -0700

    feat: `heif`: add `WriteJPEG()`, `WriteJPEGDir()`, `WriteJPEGFile()`

--- go.mod
+++ go.mod
@@ -3,24 +3,26 @@ module github.com/grokify/goimage
 go 1.18
 
 require (
+	github.com/adrium/goheif v0.0.0-20230113233934-ca402e77a786
 	github.com/andybons/gogif v0.0.0-20140526152223-16d573594812
 	github.com/chai2010/webp v1.1.1
 	github.com/disintegration/imaging v1.6.2
 	github.com/esimov/pigo v1.4.6
 	github.com/fogleman/gg v1.3.0
 	github.com/fogleman/primitive v0.0.0-20200504002142-0373c216458b
-	github.com/grokify/mogo v0.45.0
+	github.com/grokify/mogo v0.50.0
 	github.com/jessevdk/go-flags v1.5.0
 	github.com/lucasb-eyer/go-colorful v1.2.0
 	github.com/nfnt/resize v0.0.0-20180221191011-83c6a9932646
 	github.com/rustyoz/svg v0.0.0-20200706102315-fe1aeca2ba20
 	github.com/skip2/go-qrcode v0.0.0-20200617195104-da1b6568686e
-	golang.org/x/term v0.5.0
+	golang.org/x/term v0.9.0
 )
 
 require (
 	github.com/buger/jsonparser v1.1.1 // indirect
 	github.com/cheekybits/is v0.0.0-20150225183255-68e9c0620927 // indirect
+	github.com/go-logfmt/logfmt v0.6.0 // indirect
 	github.com/golang/freetype v0.0.0-20170609003504-e2365dfdc4a0 // indirect
 	github.com/google/go-querystring v1.1.0 // indirect
 	github.com/huandu/xstrings v1.4.0 // indirect
@@ -29,9 +31,11 @@ require (
 	github.com/modern-go/reflect2 v1.0.2 // indirect
 	github.com/rustyoz/Mtransform v0.0.0-20190224104252-60c8c35a3681 // indirect
 	github.com/rustyoz/genericlexer v0.0.0-20190224115003-eb82fd2987bd // indirect
+	github.com/rwcarlsen/goexif v0.0.0-20190401172101-9e8deecbddbd // indirect
 	github.com/valyala/fastjson v1.6.4 // indirect
-	golang.org/x/exp v0.0.0-20230113213754-f9f960f08ad4 // indirect
-	golang.org/x/image v0.3.0 // indirect
-	golang.org/x/sys v0.5.0 // indirect
-	golang.org/x/text v0.6.0 // indirect
+	golang.org/x/exp v0.0.0-20230522175609-2e198f4a06a1 // indirect
+	golang.org/x/image v0.8.0 // indirect
+	golang.org/x/sys v0.9.0 // indirect
+	golang.org/x/text v0.10.0 // indirect
+	gopkg.in/oleiade/reflections.v1 v1.0.0 // indirect
 )
---
+++ heif/heif.go
@@ -0,0 +1,101 @@
+package heif
+
+import (
+	"errors"
+	"image/jpeg"
+	"io"
+	"os"
+	"path/filepath"
+	"regexp"
+
+	"github.com/adrium/goheif"
+	goheifheif "github.com/adrium/goheif/heif"
+	"github.com/grokify/mogo/errors/errorsutil"
+	"github.com/grokify/mogo/image/imageutil"
+	"github.com/grokify/mogo/io/ioutil"
+	"github.com/grokify/mogo/os/osutil"
+	"github.com/grokify/mogo/path/filepathutil"
+)
+
+var rxHEICExtension = regexp.MustCompile(`(?i)\.heic$`)
+
+// WriteJPEGDir uses `imageutil.JPEGEncodeOptions` properties `WriteExtension`, and `Options`.
+func WriteJPEGDir(outdir, srcdir string, rxHEICFilename *regexp.Regexp, force, recurse bool, opt *imageutil.JPEGEncodeOptions) error {
+	if opt == nil {
+		opt = &imageutil.JPEGEncodeOptions{}
+	}
+	if rxHEICFilename == nil {
+		rxHEICFilename = rxHEICExtension
+	}
+	entries, err := osutil.ReadDirMore(srcdir, rxHEICFilename, false, true, false)
+	if err != nil {
+		return err
+	}
+	ext := opt.WriteExtensionOrDefault()
+
+	for _, e := range entries {
+		heicfilename := filepath.Join(srcdir, e.Name())
+		jpegfilename := filepath.Join(outdir, filepathutil.TrimExt(e.Name())+ext)
+		if !force {
+			if ok, err := osutil.IsFile(jpegfilename, true); err == nil && ok {
+				continue
+			}
+		}
+		if err := WriteJPEGFile(jpegfilename, heicfilename, opt.Options); err != nil {
+			return err
+		}
+	}
+
+	if recurse {
+		sdirs, err := osutil.ReadDirMore(srcdir, nil, true, false, false) // call again to avoid using regexp match on subdirs.
+		if err != nil {
+			return err
+		}
+		for _, sdir := range sdirs {
+			if err := WriteJPEGDir(filepath.Join(outdir, sdir.Name()), filepath.Join(srcdir, sdir.Name()), rxHEICFilename, force, recurse, opt); err != nil {
+				return err
+			}
+		}
+	}
+	return nil
+}
+
+func WriteJPEGFile(jpegName, heicName string, opt *jpeg.Options) error {
+	fi, err := os.Open(heicName)
+	if err != nil {
+		return err
+	}
+	defer fi.Close()
+
+	fo, err := os.OpenFile(jpegName, os.O_WRONLY|os.O_CREATE, 0600)
+	if err != nil {
+		return err
+	}
+	defer fo.Close()
+	return WriteJPEG(fo, fi, opt)
+}
+
+func WriteJPEG(w io.Writer, r ioutil.AtReader, opt *jpeg.Options) error {
+	exif, err := goheif.ExtractExif(r)
+	if err != nil {
+		if errors.Is(err, goheifheif.ErrNoEXIF) {
+			exif = []byte{}
+		} else {
+			return errorsutil.Wrap(err, "goheif.ExtractExif() error: ")
+		}
+	}
+
+	img, err := goheif.Decode(r)
+	if err != nil {
+		return errorsutil.Wrap(err, "failed to parse HEIC io.Reader using goheif.Decode()")
+	}
+
+	err = imageutil.WriteJPEG(w, img, &imageutil.JPEGEncodeOptions{
+		Options: opt,
+		Exif:    exif,
+	})
+	if err != nil {
+		return errorsutil.Wrap(err, "failed to write JPEG to io.Writer using imageutil.WriteJPEG()")
+	}
+	return nil
+}
